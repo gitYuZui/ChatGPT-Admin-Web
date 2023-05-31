d90fdda846700f8dfdc2501cf34de6582794f140
zzz <1170941265@qq.com>
2023-05-31 11:11:28 +0800

    test

--- packages/database/src/redis/client.ts
+++ packages/database/src/redis/client.ts
@@ -1,5 +1,9 @@
 import { Redis } from "@upstash/redis";
 
+
+console.error("redis-url", process.env.REDIS_URL ?? "");
+console.error("REDIS_TOKEN", process.env.REDIS_TOKEN ?? "");
+
 export const defaultRedis = new Redis({
   url: process.env.REDIS_URL ?? "",
   token: process.env.REDIS_TOKEN ?? "",
